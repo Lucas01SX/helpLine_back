a046cc2593247b28163999e69da47c46c91ec4fb
Lucas dos Santos Santana <p566873@corp.caixa.gov.br>
2025-01-22 11:08:58 -0300

    Atualização do login e filas

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,10 +3,10 @@ import { UserService } from '../services/UserService';
 
 export class UserController {
     public async validacaoUsuario(req: Request, res: Response): Promise<void> {
-        const { matricula, senha } = req.body;
+        const { matricula, senha, socketId } = req.body;
         const mat = parseInt(matricula);
         try {
-            const user = await UserService.autenticacao(mat, senha);
+            const user = await UserService.autenticacao(mat, senha, socketId);
             res.status(200).json({ message: 'Autenticação bem-sucedida!', user });
         } catch (error) {
             if (error instanceof Error) {
@@ -40,6 +40,19 @@ export class UserController {
             } else {
                 res.status(400).json({ message: 'As senhas não conferem, por gentileza verifique!' }); 
             }
+        } catch(error) {
+            if (error instanceof Error) {
+                res.status(400).json({ message: error.message }); 
+            } else {
+                res.status(500).json({ message: 'Erro desconhecido na controller', error });
+            }
+        }
+    }
+    public async deslogar(req:Request, res:Response):Promise <void> {
+        const { socketId } = req.body;
+        try {
+            const logoff = await UserService.deslog_suporte(socketId);
+            res.status(200).json({message: 'Logoff realizado', logoff})
         } catch(error) {
             if (error instanceof Error) {
                 res.status(400).json({ message: error.message }); 
--- src/services/FilasServices.ts
+++ src/services/FilasServices.ts
@@ -12,4 +12,17 @@ export class FilasService {
             throw e;
         }
     }
+    public static async consultaSkill(matricula:number): Promise<any> {
+        try {
+            const res = await pool.query(`select matricula, login, string_agg(mcdu,',') mcdu, string_agg(fila,',') fila from suporte.tb_skills_staff where excluida <> true and matricula = $1 group by matricula, login`,[matricula]);
+            if (res.rows.length === 0) {
+                throw new Error('Usuário não encontrado');
+            } 
+            const skills = res.rows[0];
+            return skills;
+        } catch (e) {
+            console.error('Erro na busca das skills:', e);
+            throw e;
+        }
+    }
 }
