c55b7fbefc5dfdad49d6c629a393c4af0a83b988
Lucas dos Santos Santana <mauricio.ferrari@lucasplansul.com.br>
2025-02-17 12:12:04 +0000

    Update DashboardService.ts

--- src/services/DashboardService.ts
+++ src/services/DashboardService.ts
@@ -15,7 +15,7 @@ export class DashboardService {
 
   private static async usuariosLogadosDash(): Promise<any> {
     try {
-      const result = await pool.query(`SELECT DISTINCT ON (a.pk_id_usuario) a.pk_id_usuario, string_agg(DISTINCT d.segmento, ',') AS segmento, string_agg(DISTINCT d.mcdu::text, ',') AS mcdu, string_agg(DISTINCT d.fila, ',') AS fila, a.hr_login AS hr_login, a.hr_logoff AS hr_logoff  FROM suporte.tb_login_logoff_suporte a  JOIN suporte.tb_login_suporte b ON a.pk_id_usuario = b.id_usuario  JOIN suporte.tb_skills_staff c ON b.matricula = c.matricula::int  JOIN trafego.tb_anexo1g d ON c.mcdu::int = d.mcdu  WHERE a.dt_login = CURRENT_DATE  GROUP BY a.pk_id_usuario, a.hr_login, a.hr_logoff  ORDER BY a.pk_id_usuario, a.hr_login DESC`);
+      const result = await pool.query(`SELECT DISTINCT ON (a.pk_id_usuario) a.pk_id_usuario, STRING_AGG(DISTINCT d.segmento, ',') AS segmento, STRING_AGG(DISTINCT d.mcdu::TEXT, ',') AS mcdu, STRING_AGG(DISTINCT d.fila, ',') AS fila, a.hr_login AS ultimo_login, a.hr_logoff AS ultimo_logoff FROM suporte.tb_login_logoff_suporte a JOIN suporte.tb_login_suporte b ON a.pk_id_usuario = b.id_usuario JOIN suporte.tb_skills_staff c ON b.matricula = c.matricula::INT JOIN trafego.tb_anexo1g d ON c.mcdu::INT = d.mcdu WHERE a.dt_login = CURRENT_DATE group by A.pk_id_usuario, a.id_login ORDER BY a.pk_id_usuario, a.id_login desc`);
       return result.rows.map((usuario: any) => ({
         id: usuario.pk_id_usuario,
         segmento: usuario.segmento,
